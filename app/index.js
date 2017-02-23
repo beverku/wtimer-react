5f1d9d874508a782b8deecf9847ef747bc9ad056
Ryan Bever <rbever@netapp.com>
2017-02-22 19:57:25 -0700

    Basic AmrapTimer

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
 import Layout from './components/Layout';
 import Timer from './components/Timer';
+import AmrapTimer from './components/AmrapTimer';
 import Settings from './components/Settings';
 
 const app = document.getElementById('app');
@@ -13,7 +14,7 @@ ReactDOM.render(
         <Route path="/" component={Layout}>
             <IndexRedirect to="/fortime" />
             <Route path="/fortime" component={Timer} />
-            <Route path="/amrap" component={Timer} />
+            <Route path="/amrap" component={AmrapTimer} />
             <Route path="/tabata" component={Timer} />
             <Route path="/settings" component={Settings} />
         </Route>
